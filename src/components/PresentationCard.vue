6def3eac0cde0f4197b5ab6ec48029346757fc98
Adrian Herrera <raiman00@hotmail.com>
2019-10-10 20:29:09 -0400

    Photos and info added in "Nosotros" tab

--- src/components/PresentationCard.vue
+++ src/components/PresentationCard.vue
@@ -2,14 +2,14 @@
   <div class="company__card" id="card" @click="flipcard()">
     <div class="company__inner" :style="{ transform: flip }">
       <div class="company__img">
-        <img :src="foto" alt="Foto" />
+        <img :src="require(`@/images/photos/${foto}`)" alt="Foto" />
         <ul class="company__name" id="name">
           <p style="margin: auto;">{{ name }}</p>
         </ul>
       </div>
       <div class="company__data">
         <p class="data_name">{{ name }}</p>
-        <p class="data_info">{{ info }}</p>
+        <p class="data_info" :style="InfoSize">{{ info }}</p>
       </div>
     </div>
   </div>
@@ -33,6 +33,17 @@ export default {
         this.flip = "rotateY(180deg)";
       } else if (this.flip == "rotateY(180deg)") {
         this.flip = "none";
+      }
+    }
+  },
+  computed: {
+    InfoSize() {
+      if (this.info.length > 450) {
+        return {
+          fontSize: ".9rem"
+        };
+      } else {
+        return { fontSize: "1rem" };
       }
     }
   }
